19cb4123c47a3796fe0638c1828c3c36083ec0db
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-05-03 18:14:32 +0200

    Task-01 built

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const colorStart = document.querySelector("button[data-start]");
+const colorStop = document.querySelector("button[data-stop]");
+let timerId = null;
+
+colorStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+colorStop.addEventListener("click", () => {
+    clearInterval(timerId);
+});
